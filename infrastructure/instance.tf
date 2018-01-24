4bf61124e3bcb4aa0829298a11a5740326899001
Rohit Singh <rohit.t.kumar.singh@pwc.com>
2018-01-24 13:23:39 +0530

    Added terraform scripts

---
+++ infrastructure/instance.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "default"
+}
+
+resource "aws_instance" "pwc_demo_instances" {
+  count = "1"
+  instance_type          = "t2.large"
+  ami                    = "ami-ca4c1da5"
+  key_name               = "PWC-DEMO-K"
+  availability_zone      = "ap-south-1a"
+  subnet_id              = "subnet-bed470d6"
+}
+
+
+output "public_ip" {
+  value = "${aws_instance.pwc_demo_instances.public_ip}"
+}
---
+++ wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ -f "/var/tmp/publicip" ];then
+rm -f /var/tmp/publicip
+fi
+
+echo "Deploying Demo Instance"
+
+cd infrastructure
+terraform init
+terraform plan -out demo.plan
+terraform apply -auto-approve "demo.plan"
+terraform output public_ip > /var/tmp/publicip
+echo "Instance Created Successfully"
